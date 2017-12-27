295479095795db2c565a5dd867e157e18eb9d1f5
erick <erick@hotmail.ca>
2017-12-27 02:30:29 -0500

    Enhance promises with async/await syntax

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const fetch = require('node-fetch');
 const Graph = require('./graph.js');
 
-const menuGraph = new Graph();
-
-async function getMatches(page=1, id=1) {
+async function getMatches(id, page) {
   let response = await fetch(`https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=${id}&page=${page}`);
   let data = await response.json();
   return data;
 }
 
-function addToMenuGraph(menus) {
+function addToMenuGraph(graph, menus) {
   menus.forEach(menu => {
-    menuGraph.addVertex(menu.id, menu.child_ids);
+    graph.addVertex(menu.id, menu.child_ids);
   })
 }
 
@@ -19,20 +17,21 @@ function countPages(itemsPerPage, totalItems) {
   return Math.ceil(totalItems/itemsPerPage);
 }
 
-getMatches()
-  .then(result => {
-    addToMenuGraph(result.menus);
-    const numPages = countPages(result.pagination.per_page, result.pagination.total);
-    const promises = [];
-    for (let i=2; i<=numPages; i++) {
-      promises.push(getMatches(i));
-    }
-    return Promise.all(promises);
-  })
-  .then(results => {
-    const menus = results.reduce((acc,val) => acc.concat(val.menus), []);
-    addToMenuGraph(menus);
-  })
-  .catch(error => {
-    console.log(`ERROR: ${error}`);
-  });
+async function initializeGraph(caseId) {
+  const menuGraph = new Graph();
+  const firstPage = await getMatches(caseId, 1);
+  addToMenuGraph(menuGraph, firstPage.menus);
+
+  const numPages = countPages(firstPage.pagination.per_page, firstPage.pagination.total);
+
+  const promises = [];
+  for (let i=2; i<=numPages; i++) {
+    promises.push(getMatches(caseId, i));
+  }
+  const restOfPages = await Promise.all(promises);
+  const restOfMenus = restOfPages.reduce((acc,val) => acc.concat(val.menus), []);
+  addToMenuGraph(menuGraph, restOfMenus);
+  return menuGraph;
+}
+
+
